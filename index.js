88aea7b465c44106d9e5701c618889dc78b57b06
sanju-88 <sanjusawariya88@gmail.com>
2026-03-31 01:08:30 +0530

    backend-update

--- index.js
+++ index.js
@@ -1,30 +1,9 @@
 import express from "express";
-import ConnectDb from "./config/db.js";
-import authRouter from "./routes/auth.routes.js";
-import userRouter from "./routes/user.routes.js";
-import cors from "cors";
-import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(express.json());
-app.use(
-  cors({
-    origin: "*",
-    credentials: true,
-  }),
-);
-app.use(cookieParser());
-app.use(`/api/auth`, authRouter);
-app.use(`/api/user`, userRouter);
-
-
-
-// app.listen(port, () => {
-//   ConnectDb();
-//   console.log("server started");
-// });
-
 app.get("/", (req, res) => {
-  res.send("API running 🚀");
+  res.send("Server working ✅");
 });
+
+export default app;
